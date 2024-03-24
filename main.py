f175c3130847b0af0ff4d368c92970fc754a8a8d
STBrian <brichap100@gmail.com>
2024-03-24 16:31:47 -0600

    Convert from bjson to json sucessfully

--- main.py
+++ main.py
@@ -1,13 +1,43 @@
 import json, sys
 from conversions import *
 from pathlib import Path
-from JOAAThash import *
+from JOAAThash import getLittleJOAAThash
 
 def extract_chunk(data: bytes, idx: int, size: int = 4, start_from: int = 0):
     start = idx * size + start_from
     return data[start:start + size]
 
-filepath = Path("def_action_test.bjson")
+def getHeaders(data: bytes):
+    text_region_start = (int.from_bytes(extract_chunk(data, 0), "little", signed=False) * 3 * 4) + 4
+    lenght_text_r = int.from_bytes(extract_chunk(data, 0, 4, text_region_start), "little", signed=False)
+    region_start = text_region_start + lenght_text_r + 4
+    pre_region_len = int.from_bytes(extract_chunk(data, 0, 4, region_start), "little", signed=False)
+    lenght = int.from_bytes(extract_chunk(data, pre_region_len + 1, 4, region_start), "little", signed=False)
+    print(pre_region_len)
+    print(lenght)
+    headers = [""] * lenght
+    headers_text_start = region_start + (pre_region_len + 1) * 4 + (lenght) * 4 * 3 + 4
+    for i in range(lenght):
+        idx = pre_region_len + 1 + (i) * 3 + 1
+        headers_idx = int.from_bytes(extract_chunk(data, idx + 1, 4, region_start), "little", signed=False)
+        headers_idx_end = headers_text_start + 4 + headers_idx
+        while True:
+            if data[headers_idx_end] == 0:
+                break
+            else:
+                headers_idx_end += 1
+        header_part = data[headers_text_start + 4 + headers_idx:headers_idx_end]
+        header_idx = int.from_bytes(extract_chunk(data, idx + 2, 4, region_start), "little", signed=False) # Nota es distinto a headers_idx
+        if header_idx <= lenght:
+            headers[header_idx - 1] = header_part.decode('utf-8')
+        else:
+            for _ in range(header_idx - len(headers)):
+                headers.append("")
+            headers[header_idx - 1] = header_part.decode('utf-8')
+    return headers
+
+#filepath = Path(input("Introduce the name of the file: "))
+filepath = Path("blocks.bjson")
 with open(filepath, "rb") as f:
     data_bytes = f.read()
 
@@ -16,8 +46,8 @@ place_dir = []
 
 text_region_idx = int.from_bytes(extract_chunk(data_bytes, 0), "little", signed=False) * 3 + 1
 lenght_text_region = int.from_bytes(extract_chunk(data_bytes, text_region_idx), "little", signed=False)
-after_region_start = ((text_region_idx + 1) * 4) + lenght_text_region
-print(after_region_start)
+headers = getHeaders(data_bytes)
+print(headers)
 #sys.exit()
 
 for i in range(int.from_bytes(extract_chunk(data_bytes, 0), "little", signed=False)):
@@ -36,9 +66,11 @@ for i in range(int.from_bytes(extract_chunk(data_bytes, 0), "little", signed=Fal
             tmp = place_dir[-1]
             dir = tmp[0]
             if tmp[1] == "array":
-                dir[f"dummy{i}"] = {}
+                # Header
+                dir[f"{headers[i-1]}"] = {}
                 tmp2 = []
-                tmp2.append(dir[f"dummy{i}"])
+                # Header in tmp
+                tmp2.append(dir[f"{headers[i-1]}"])
                 tmp2.append("array")
                 tmp2.append(int.from_bytes(extract_chunk(data_bytes, idx + 1), "little", signed=False))
                 tmp2.append(0)
@@ -66,7 +98,8 @@ for i in range(int.from_bytes(extract_chunk(data_bytes, 0), "little", signed=Fal
                 else:
                     text_idx_end += 1
             text_part = data_bytes[((text_region_idx + 1) * 4) + text_idx:text_idx_end]
-            dir[f"dummy{i}"] = text_part.decode("utf-8")
+            # Header
+            dir[f"{headers[i-1]}"] = text_part.decode("utf-8")
         elif tmp[1] == "list":
             text_idx = int.from_bytes(extract_chunk(data_bytes, idx + 2), "little", signed=False)
             text_idx_end = ((text_region_idx + 1) * 4) + text_idx
@@ -81,9 +114,11 @@ for i in range(int.from_bytes(extract_chunk(data_bytes, 0), "little", signed=Fal
     elif type_extracted == 4:
         tmp = place_dir[-1]
         dir = tmp[0]
-        dir[f"dummy{i}"] = []
+        # Header
+        dir[f"{headers[i-1]}"] = []
         tmp2 = []
-        tmp2.append(dir[f"dummy{i}"])
+        # Header in tmp
+        tmp2.append(dir[f"{headers[i-1]}"])
         tmp2.append("list")
         tmp2.append(int.from_bytes(extract_chunk(data_bytes, idx + 1), "little", signed=False))
         tmp2.append(0)
@@ -96,7 +131,8 @@ for i in range(int.from_bytes(extract_chunk(data_bytes, 0), "little", signed=Fal
         dir = tmp[0]
         if tmp[1] == "array":
             float_num = bytes_to_float(extract_chunk(data_bytes, idx + 1), "little")
-            dir[f"dummy{i}"] = float("{:.2f}".format(float_num))
+            # Header
+            dir[f"{headers[i-1]}"] = float("{:.2f}".format(float_num))
         elif tmp[1] == "list":
             float_num = bytes_to_float(extract_chunk(data_bytes, idx + 1), "little")
             dir.append(float("{:.2f}".format(float_num)))
@@ -105,7 +141,8 @@ for i in range(int.from_bytes(extract_chunk(data_bytes, 0), "little", signed=Fal
         tmp = place_dir[-1]
         dir = tmp[0]
         if tmp[1] == "array":
-            dir[f"dummy{i}"] = bytes_to_int(extract_chunk(data_bytes, idx + 1), "little")
+            # Header
+            dir[f"{headers[i-1]}"] = bytes_to_int(extract_chunk(data_bytes, idx + 1), "little")
         elif tmp[1] == "list":
             dir.append(bytes_to_int(extract_chunk(data_bytes, idx + 1), "little"))
         tmp[3] += 1
@@ -114,10 +151,11 @@ for i in range(int.from_bytes(extract_chunk(data_bytes, 0), "little", signed=Fal
         dir = tmp[0]
         if tmp[1] == "array":
             bool_num = int.from_bytes(extract_chunk(data_bytes, idx + 1), "little", signed=False)
+            # With header
             if bool_num == 0:
-                dir[f"dummy{i}"] = False
+                dir[f"{headers[i-1]}"] = False
             elif bool_num == 1:
-                dir[f"dummy{i}"] = True
+                dir[f"{headers[i-1]}"] = True
         elif tmp[1] == "list":
             bool_num = int.from_bytes(extract_chunk(data_bytes, idx + 1), "little", signed=False)
             if bool_num == 0:
