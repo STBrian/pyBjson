1a355e2a3eae125164800124fc3660e61e258f48
STBrian <brichap100@gmail.com>
2024-09-04 22:14:42 -0600

    rewritting

--- bjson.py
+++ bjson.py
@@ -78,7 +78,7 @@ class BJSONHashDatabase():
             else:
                 return None
 
-def getHeaders(data: bytes, hash_database: BJSONHashDatabase):
+def getBJSONHeaders(data: bytes, hash_database: BJSONHashDatabase):
     global debug_messages
 
     text_region_start = (int.from_bytes(extract_chunk(data, 0), "little", signed=False) * 3 * 4) + 4
@@ -122,6 +122,67 @@ def getHeaders(data: bytes, hash_database: BJSONHashDatabase):
 
     return headers
 
+def getObject(object: dict | list, file_data, n_objects: int, headers, text_region_start) -> int:
+    if type(object) == dict or type(object) == list:
+        new_object = {}
+        idx = n_objects * 3 + 1
+        structure_lenght = int.from_bytes(extract_chunk(file_data, idx + 1), "little", signed=False)
+        if type(object) == dict:
+            new_object_header = headers[n_objects]
+        n_objects += 1
+
+        for i in range(structure_lenght):
+            idx = n_objects * 3 + 1
+
+            data_type = int.from_bytes(extract_chunk(file_data, idx), "little", signed=False)
+            if data_type == 6:
+                # JSON Object type
+                n_objects = getObject(new_object, file_data, n_objects, headers, text_region_start)
+            elif data_type == 5:
+                # JSON String type
+                n_objects = getString(new_object, file_data, n_objects, headers, text_region_start)
+            elif data_type == 4:
+                # JSON Array type
+                pass
+
+        if type(object) == dict:
+            object[new_object_header] = new_object
+        elif type(object) == list:
+            object.append(new_object)
+        else:
+            raise TypeError("'object' must be dict or list")
+    
+        return n_objects
+    else:
+        raise TypeError("'object' must be dict or list")
+
+def getString(object: dict | list, file_data: bytes, n_objects: int, headers: list, text_region_start: int) -> int:
+    if type(object) == dict or type(object) == list:
+        idx = n_objects * 3 + 1
+        if type(object) == dict:
+            object_header = headers[n_objects]
+        n_objects += 1
+
+        text_idx = int.from_bytes(extract_chunk(file_data, idx + 2), "little", signed=False)
+        text_start = text_region_start + text_idx
+        text_end = text_start
+        while (file_data[text_end] != 0):
+            text_end += 1
+
+        text_raw = file_data[text_start:text_end]
+        text_decode = text_raw.decode('utf-8')
+
+        if type(object) == dict:
+            object[object_header] = text_decode
+        elif type(object) == list:
+            object.append(text_decode)
+        else:
+            raise TypeError("'object' must be dict or list")
+    
+        return n_objects
+    else:
+        raise TypeError("'object' must be dict or list")
+
 class BJSONEntry:
     def __init__(self):
         pass
@@ -149,5 +210,33 @@ class BJSON:
         if debug_messages:
             print("[Info] Getting BJSON headers")
 
-        headers = getHeaders(file_bytes, hash_db)
-        text_region_start = (int.from_bytes(extract_chunk(file_bytes, 0), "little", signed=False) * 3 * 4) + 4
+        headers = getBJSONHeaders(file_bytes, hash_db)
+        n_structure_objects = int.from_bytes(extract_chunk(file_bytes, 0), "little", signed=False)
+        text_region_start = (n_structure_objects * 3 * 4) + 8
+
+        if debug_messages:
+            print("[Info] Getting BJSON structure")
+
+        data_type = int.from_bytes(extract_chunk(file_bytes, 1), "little", signed=False)
+        if data_type == 6:
+            # JSON Object type
+            content_struct = {}
+            structure_lenght = int.from_bytes(extract_chunk(file_bytes, 2), "little", signed=False)
+        elif data_type == 4:
+            # JSON Array type
+            content_struct = []
+            structure_lenght = int.from_bytes(extract_chunk(file_bytes, 2), "little", signed=False)
+        else:
+            raise Exception("Unexpected structure opening value")
+        
+        n_objects = 1
+        for i in range(structure_lenght):
+            idx = n_objects * 3 + 1
+
+            data_type = int.from_bytes(extract_chunk(file_bytes, idx), "little", signed=False)
+            if data_type == 6:
+                # JSON Object type
+                n_objects = getObject(content_struct, file_bytes, n_objects, headers, text_region_start)
+            elif data_type == 5:
+                # JSON String type
+                n_objects = getString(content_struct, file_bytes, n_objects, headers, text_region_start)
