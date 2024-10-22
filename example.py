b0509fc6fe370004c9f9a5decbb7311ecdec16c4
STBrian <brichap100@gmail.com>
2024-10-21 19:36:25 -0600

    testing with example

--- example.py
+++ example.py
@@ -3,14 +3,14 @@ from bjsontojson_legacy import convertBjsonToJson_legacy
 
 file = "mobs"
 
-json_str = convertBjsonToJson_legacy(f"./{file}.bjson")
+'''json_str = convertBjsonToJson_legacy(f"./{file}.bjson")
 
 if not json_str:
     print("Cannot convert bjson to json")
 else:
     with open(f"./{file}.json", "w", encoding="utf-8") as f:
-        f.write(json_str)
+        f.write(json_str)'''
 
-    success = convertJsonToBjson_legacy(f"./{file}.json")
-    if not success[0]:
-        print("Cannot convert json to bjson")
+success = convertJsonToBjson_legacy(f"./{file}.json")
+if not success[0]:
+    print("Cannot convert json to bjson")
