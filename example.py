ff0eddb44b0b615d370d1382d0d4608447f198a5
STBrian <brichap100@gmail.com>
2024-12-27 20:51:32 -0600

    Testing

--- example.py
+++ example.py
@@ -1,17 +1,16 @@
 from bjson import BJSONFile
-from jsontobjson_legacy import convertJsonToBjson_legacy
 
-filename = "your_file"
+filename = "mobs"
 
-bjson_file = BJSONFile().open(f"./{filename}.bjson")
+'''bjson_file = BJSONFile().open(f"./{filename}.bjson")
 json_str = bjson_file.toJson(showDebug=True)
 
 with open(f"./{filename}.json", "w", encoding="utf-8") as f:
-    f.write(json_str)
+    f.write(json_str)'''
 
-bjson_file.fromJson(json_str)
+with open(f"{filename}.json", "r") as f:
+    bjson_file = BJSONFile()
+    bjson_file.fromJson(f.read())
 
 with open(f"./{filename}_converted_new.bjson", "wb") as f:
     f.write(bjson_file.getData())
-
-convertJsonToBjson_legacy(f"./{filename}.json")
